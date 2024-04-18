7f6c4809d645633c537a0c72a856c38853f7d2a0
danineiverth17 <105320005+danineiverth17@users.noreply.github.com>
2024-04-18 08:41:17 -0300

    ok

--- main.js
+++ main.js
@@ -33,8 +33,11 @@ function calculaTempo(tempoObjetivo) {
     segundos %= 60;
     minutos %= 60;
     horas %= 24;
- 
+  if (tempoFinal > 0) {
  return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos";
+} else{
+    return "Prazo Finalizado";
+}
 }
 
 function atualizaCronometro(){
